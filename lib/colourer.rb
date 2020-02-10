07179ef1397ed45fbce7b41cf8afdf75b7958f72
BertZZ <bertie.wooles@gmail.com>
2020-02-10 16:50:28 +0000

    Guards against bad arguments in input file

--- lib/colourer.rb
+++ lib/colourer.rb
@@ -1,11 +1,13 @@
 class Colourer
 
   def self.colour_cell(array,n1,n2,colour)
+    check_inputs(n1,n2)
     array[n2.to_i-1][n1.to_i-1] = colour
     array
   end
 
   def self.colour_column(array,col,n1,n2,colour)
+    check_inputs(n1,n2,col)
     array[n1.to_i-1..n2.to_i-1].each do |array|
       array[col.to_i-1] = colour
     end
@@ -13,8 +15,21 @@ class Colourer
   end
 
   def self.colour_row(array,n1,n2,row,colour)
+    check_inputs(n1,n2,row)
     array[row.to_i-1].fill(colour,(n1.to_i-1)..(n2.to_i-1))
     array
   end
 
+  private
+
+  def self.check_inputs(*args)
+    args.each do |a|
+      if a.to_i > 0
+        next
+      else
+        puts "Please check your file and enter valid arguments"
+        exit
+      end
+    end
+  end
 end
