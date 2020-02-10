516feb1dee04ba994dbaa2a949c8068a0044515f
BertZZ <bertie.wooles@gmail.com>
2020-02-10 17:39:16 +0000

    Creates methods to stop attempted colouring of non existant cells

--- lib/colourer.rb
+++ lib/colourer.rb
@@ -2,12 +2,15 @@ class Colourer
 
   def self.colour_cell(array,n1,n2,colour)
     check_inputs(n1,n2)
+    check_row_length(array,n1,n2)
+    check_column_length(array,n1,n2)
     array[n2.to_i-1][n1.to_i-1] = colour
     array
   end
 
   def self.colour_column(array,col,n1,n2,colour)
     check_inputs(n1,n2,col)
+    check_column_length(array,n1,n2)
     array[n1.to_i-1..n2.to_i-1].each do |array|
       array[col.to_i-1] = colour
     end
@@ -15,6 +18,7 @@ class Colourer
   end
 
   def self.colour_row(array,n1,n2,row,colour)
+    check_row_length(array,n1,n2)
     check_inputs(n1,n2,row)
     array[row.to_i-1].fill(colour,(n1.to_i-1)..(n2.to_i-1))
     array
@@ -28,6 +32,24 @@ class Colourer
         next
       else
         puts "Please check your file and enter valid arguments"
+        exit
+      end
+    end
+  end
+
+  def self.check_column_length(array, *args)
+    args.each do |a|
+      if a.to_i > array.length
+        puts "This cell does not exist. Please check your inputs"
+        exit
+      end
+    end
+  end
+
+  def self.check_row_length(array, *args)
+    args.each do |a|
+      if a.to_i > array[0].length
+        puts "This cell does not exist. Please check your inputs"
         exit
       end
     end
