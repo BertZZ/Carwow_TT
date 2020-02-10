71cb5f6f6a001ef3a45e0bf7cfe123e3d3e61130
BertZZ <bertie.wooles@gmail.com>
2020-02-10 15:07:50 +0000

    Creates Colour column method

--- lib/colourer.rb
+++ lib/colourer.rb
@@ -1,6 +1,15 @@
 class Colourer
-  def self.colour_cell(array,n1,n2,col)
-    array[n2.to_i-1][n1.to_i-1] = col
+
+  def self.colour_cell(array,n1,n2,colour)
+    array[n2.to_i-1][n1.to_i-1] = colour
     array
   end
+
+  def self.colour_column(array,col,n1,n2,colour)
+    array[n1.to_i-1..n2.to_i-1].each do |array|
+      array[col.to_i-1] = colour
+    end
+    array
+  end
+
 end
--- spec/colourer_spec.rb
+++ spec/colourer_spec.rb
@@ -4,8 +4,23 @@ describe Colourer do
   describe '#colour_cell' do
     it 'colours a specific cell of the bitmap' do
       array = [["O", "O", "O"], ["O", "O", "O"], ["O", "O", "O"], ["O", "O", "O"]]
-      array = Colourer.colour_cell(array,1,3,'T')
+      array = Colourer.colour_cell(array,'1','3','T')
       expect(array).to eq([["O", "O", "O"], ["O", "O", "O"], ["T", "O", "O"], ["O", "O", "O"]])
     end
   end
+
+  describe '#colour_column' do
+    it 'colours the whole column' do
+      array = [["O", "O", "O"], ["O", "O", "O"], ["O", "O", "O"], ["O", "O", "O"]]
+      array = Colourer.colour_column(array,'2','1','4','T')
+      expect(array).to eq([["O", "T", "O"], ["O", "T", "O"], ["O", "T", "O"], ["O", "T", "O"]])
+    end
+
+    it 'colours the part of the column' do
+      array = [["O", "O", "O"], ["O", "O", "O"], ["O", "O", "O"], ["O", "O", "O"]]
+      array = Colourer.colour_column(array,'2','2','3','T')
+      expect(array).to eq([["O", "O", "O"], ["O", "T", "O"], ["O", "T", "O"], ["O", "O", "O"]])
+    end
+  end
+
 end
