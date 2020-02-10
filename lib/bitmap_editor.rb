6de339974e79140d2298666763e28ceb0a099aae
BertZZ <bertie.wooles@gmail.com>
2020-02-10 14:26:18 +0000

    Creates a white bitmap when given I command

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -11,7 +11,9 @@ class BitmapEditor
 
     File.open(file).each do |line|
       line = line.chomp.split
-      case line
+      case line[0]
+      when 'I'
+        @array = Array.new(line[2].to_i) { Array.new(line[1].to_i, 'O') }
       when 'S'
           puts "There is no image"
       else
--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -2,11 +2,23 @@ require 'spec_helper'
 require './lib/bitmap_editor'
 
 describe BitmapEditor do
-  it { is_expected.to be_a BitmapEditor }
+  subject = described_class.new
 
   it 'is initialized with an empty array' do
     subject = BitmapEditor.new
     expect(subject.array).to be_a_kind_of(Array)
     expect(subject.array).to be_empty
+  end
+
+  describe '#run' do
+    it 'creates an array of 5 rows and 6 columns when given an input I56' do
+      subject.run('create_array_test.txt')
+      expect(subject.array).to eq([["O", "O", "O", "O", "O"],
+                                   ["O", "O", "O", "O", "O"],
+                                   ["O", "O", "O", "O", "O"],
+                                   ["O", "O", "O", "O", "O"],
+                                   ["O", "O", "O", "O", "O"],
+                                   ["O", "O", "O", "O", "O"]])
+    end
   end
 end
